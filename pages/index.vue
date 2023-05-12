0fc70b43e85674883f836a041993c395e04d803e
paddy <pdd337816464@163.com>
2023-05-12 18:04:16 +0800

    Update index.vue and AppAlert.vue

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <img src="/img/logo.png" alt="react logo" />
+    <AppAlert>
+      This is an auto-imported component
+    </AppAlert>
+  </div>
+</template>
